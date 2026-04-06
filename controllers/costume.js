820bbc9e962b6a042e582e52d4286c747fcc40a2
Sreeyuth Chowdary Gunnam <s560337@nwmissouri.edu>
2026-04-06 12:46:39 -0500

    Implemented list all costumes endpoint

--- controllers/costume.js
+++ controllers/costume.js
@@ -1,8 +1,15 @@
 var Costume = require('../models/costume');
 
 // List of all Costumes
-exports.costume_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Costume list');
+exports.costume_list = async function(req, res) {
+    try {
+        theCostumes = await Costume.find();
+        res.send(theCostumes);
+    }
+    catch(err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 // For a specific Costume

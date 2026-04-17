20b432d2c6a593a68dc045b0800917234e6d0572
sreeyuth222 <s560337@nwmissouri.edu>
2026-04-17 11:49:51 -0500

    part 4 protected update route

--- routes/costume.js
+++ routes/costume.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var costume_controller = require('../controllers/costume');
 
+function secured(req, res, next) {
+	if (req.user) {
+		return next();
+	}
+	res.redirect('/login');
+}
+
 // GET costumes collection page
 router.get('/', costume_controller.costume_view_all_Page);
 
@@ -12,7 +19,7 @@ router.get('/detail', costume_controller.costume_view_one_Page);
 router.get('/create', costume_controller.costume_create_Page);
 
 // GET update costume page
-router.get('/update', costume_controller.costume_update_Page);
+router.get('/update', secured, costume_controller.costume_update_Page);
 
 // GET delete costume page
 router.get('/delete', costume_controller.costume_delete_Page);

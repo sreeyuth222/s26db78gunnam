41297053b392ee331225061ea38de142d82da0d9
sreeyuth222 <s560337@nwmissouri.edu>
2026-04-17 12:04:44 -0500

    part 5 added model validation

--- models/costume.js
+++ models/costume.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const costumeSchema = mongoose.Schema({
   costume_type: String,
   size: String,
-  cost: Number
+  cost: {
+    type: Number,
+    min: 0,
+    max: 1000
+  }
 });
 
 module.exports = mongoose.model("Costume", costumeSchema);
